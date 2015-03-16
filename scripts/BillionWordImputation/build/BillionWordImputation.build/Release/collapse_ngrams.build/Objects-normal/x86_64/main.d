6b34ae50a6b00533170195e77aa752d8013e4962
Timothy Palpant <tim@palpant.us>
2015-03-15 21:54:27 -0400

    remove build

--- scripts/BillionWordImputation/build/BillionWordImputation.build/Release/collapse_ngrams.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/timpalpant/Documents/Workspace/kaggle/BillionWordImputation/scripts/BillionWordImputation/collapse_ngrams/main.cpp
--- scripts/BillionWordImputation/build/BillionWordImputation.build/Release/count_unigrams.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/timpalpant/Documents/Workspace/kaggle/BillionWordImputation/scripts/BillionWordImputation/count_unigrams/main.cpp
